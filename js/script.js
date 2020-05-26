23cfb8d12e6c027b95fa247da3f2cd8a0236e81b
Cooper Hollmaier <cooperjhollmaier@gmail.com>
2020-05-26 12:54:25 -0700

    Added to-do for first section of code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 console.log('hello world');
+
+
+
+// TODO: Set focus on first text field
+
+// TODO: Add a text field when "other" is selected in Job Role
+
+/* REGISTER FOR ACTIVITIES */
+
+// TODO: Until a theme is selected from the "design" menu, no color options are available
+
+// TODO: Only certain color options should be available based on "design" selection
+
+// TODO: If "design" choice is changed, color and menu is updated
+
+// TODO: Don't allow selection of events happening on the same day and time if one is already selected
+
+// TODO: When competing activity is unselected, the former state should revert
+
+// TODO: Checked activities should dynamically total up at the bottom
