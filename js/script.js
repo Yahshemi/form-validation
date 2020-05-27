8752a2eae77076acb7887a72fa1c058e69996a6d
Cooper Hollmaier <cooperjhollmaier@gmail.com>
2020-05-27 16:04:12 -0700

    Other field and color logic working

--- js/script.js
+++ js/script.js
@@ -1,18 +1,45 @@
 // Global Variables
 
+// Create text field for job title = other
+let title = document.getElementById('title');
+let newField = document.createElement('input');
+newField.setAttribute('type', 'text');
+newField.style.display = 'none';
+title.parentNode.appendChild(newField);
+
 // Initial focus: Set name field as the initial focus on page load
 window.addEventListener("load", e => {
     let userName = document.getElementById('name');
     userName.focus();
+
 });
 
+// Add an event listener to hide/show extra text field when other is selected as a role
 
+title.addEventListener('click', e => {
+    if (e.target.value === 'other') {
+        newField.style.display = '';
 
-// TODO: Add a text field when "other" is selected in Job Role
+    } else
+        newField.style.display = 'none';
+});
 
-/* REGISTER FOR ACTIVITIES */
+// Create logic so that the color dropdown and it's label aren't visible until a design is selected
+let design = document.getElementById('design');
+let color = document.getElementById('color');
+color.style.display = 'none';
+color.previousElementSibling.style.display = 'none';
+design.addEventListener('click', e => {
+    if (e.target.textContent === 'Select Theme') {
+        color.style.display = 'none';
+        color.previousElementSibling.style.display = 'none';
+    } else {
+        color.style.display = '';
+        color.previousElementSibling.style.display = '';
 
-// TODO: Until a theme is selected from the "design" menu, no color options are available
+    }
+});
+
 
 // TODO: Only certain color options should be available based on "design" selection
 
