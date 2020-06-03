7b932e67f2039be3267b8a1eb075b37d577ea78d
chollma <53924848+chollma@users.noreply.github.com>
2020-06-02 22:02:04 -0700

    Working on getting certain colors to display based on selection

--- js/script.js
+++ js/script.js
@@ -29,26 +29,39 @@ let design = document.getElementById('design');
 let color = document.getElementById('color');
 color.previousElementSibling.style.display = 'none'; // By default don't display label for color
 color.style.display = 'none'; // By default don't display color drop down
+color.options[0].hidden = true;
 
-design.addEventListener("change", e=> { // Event listener to listen for change in drop down value and act accordingly
+design.addEventListener("click", e=> { // Event listener to listen for change in drop down value and act accordingly
     if (design.value === 'heart js'){
+       color.previousElementSibling.style.display = '';
+       color.style.display = '';
+       color.options.item(0).hidden = true;
+       color.options.item(1).hidden = true;
+       color.options.item(2).hidden = true;
+       color.item(3).hidden = false;
+       color.item(4).hidden = false;
+       color.item(5).hidden = false;
+       
+    } else if (design.value === 'js puns'){
         color.previousElementSibling.style.display = '';
         color.style.display = '';
-    } else if (design.value === 'js puns'){
-      color.previousElementSibling.style.display = '';
-      color.style.display = '';
+        color.item(3).hidden = true;
+        color.item(4).hidden = true;
+        color.item(5).hidden = true;
+        color.options.item(0).hidden = false;
+        color.options.item(1).hidden = false;
+        color.options.item(2).hidden = false;
     } else {
         console.log('Theme not selected');
         color.previousElementSibling.style.display = 'none'; 
         color.style.display = 'none'; 
-  
     }
 
-   }); 
+  }); 
 
 // TODO: Only certain color options should be available based on "design" selection
 
-// TODO: If "design" choice is changed, color and menu is updated
+// TODO: If "design" choice is changed, color and menu is updated - I think I've accounted for that above
 
 // TODO: Don't allow selection of events happening on the same day and time if one is already selected
 
