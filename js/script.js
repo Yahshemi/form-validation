979362dc496c45713519526e115551c258390828
Cooper Hollmaier <cooperjhollmaier@gmail.com>
2020-09-19 18:51:57 -0700

    Completed project and testing

--- js/script.js
+++ js/script.js
@@ -5,89 +5,99 @@ Intended Behavior: Interactive Form
 Goal: Exceeds Expecatations
 ******************************************/
 
-/**********************************************************
-    Setup
- *********************************************************/
+/******************************************
+SETUP
+******************************************/
+// Constructors
+let div = document.createElement('div'); // used to create a new div element
+let opt = document.createElement('option'); // used to create a new select option
+let actError = document.createElement('div');
 
-// Global variables
+// Selectors
+let form = document.querySelector('form');
 let fieldset = document.querySelectorAll('fieldset');
-let newField = document.createElement('input');
-let div = document.createElement('div'); // Create a new div and add it below the list of checkbox inputs
-let userName = document.getElementById('name');
-
-// Onload events
-window.addEventListener("load", e => {
-    userName.focus();
-    for (let i = 0; i < checkboxes.length; i++) { // Reset checkbox selection
-        checkboxes[i].checked = false;
-    }
-});
+let name = document.querySelector("#name");
+let mail = document.querySelector('#mail');
+let title = document.querySelector('#title');
+let other = document.querySelector('#other-title');
+let size = document.querySelector('#size');
+let design = document.querySelector('#design');
+let color = document.querySelector('#color');
+let colors = document.querySelector('#colors-js-puns')
+let activities = document.querySelector('.activities');
+let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+let payment = document.querySelector('#payment');
+let credit = document.querySelector('#credit-card');
+let ccNum = document.querySelector('#cc-num');
+let zip = document.querySelector('#zip');
+let cvv = document.querySelector('#cvv');
+let expMonth = document.querySelector('#exp-month');
+let expYear = document.querySelector('#exp-year');
+let paypal = document.querySelector('#paypal');
+let bitcoin = document.querySelector('#bitcoin');
 
 // Helper functions
-let hide = function() {
+let hide = function() { // iterates through a set of items and sets them to hidden
     for (let i = 0; i < arguments.length; i++) {
         arguments[i].hidden = true;
     }
 }
-let show = function() {
+let show = function() { // iterates through a set of items and sets them to show
     for (let i = 0; i < arguments.length; i++) {
         arguments[i].hidden = false;
     }
 }
 
+// Page load events
+window.addEventListener("load", e => { // 
+    name.focus(); // focuses the input on the name field on page load
+    hide(other); // hides the text field for other job roles
+});
 
-/**********************************************************
-Basic Info
-*********************************************************/
-// Show and hide a text area based on selection of 'user-title'
-let title = document.getElementById('title');
-
-newField.setAttribute('type', 'text');
-hide(newField);
-title.parentNode.appendChild(newField);
+/******************************************
+    BASIC INFO
+******************************************/
 
 title.addEventListener('change', e => {
     if (e.target.value === 'other') {
-        show(newField);
+        show(other);
     } else
-        hide(newField);
+        hide(other);
 });
-/**********************************************************
-T-shirt Info
- ********************************************************/
-let design = document.getElementById('design');
-let color = document.getElementById('color');
-let colorDiv = document.getElementById('colors-js-puns');
-let colorOptions = colorDiv.querySelectorAll('option');
-let addOption = document.createElement('option');
-let colorSelect = colorDiv.querySelector('select');
-colorSelect.prepend(addOption);
-addOption.text = 'Please select a T-shirt theme';
-addOption.value = 'default';
-colorSelect.value = 'default';
-hide(colorDiv);
 
-design.addEventListener('change', e => { // Listen for change in drop down value and act accordingly
+/******************************************
+T-SHIRT INFO
+******************************************/
+
+opt.appendChild(document.createTextNode('Please select a T-shirt theme')); // If no theme is selected, the color drop down has no options
+opt.value = null;
+color.insertBefore(opt, color.childNodes[0]);
+let colorOptions = colors.querySelectorAll('option');
+hide(colors);
+color.selectedIndex = 0;
+for (let i = 1; i < colorOptions.length; i++) {
+    hide(colorOptions[i]);
+}
+
+design.addEventListener('change', e => { // Listen for change in drop down value and show colors available for selected theme
+    colorOptions[0].innerText = 'Please select a color';
+    color.selectedIndex = 0;
+    show(colors);
     if (design.value === 'heart js') {
-        show(colorDiv);
-        color.value = 'default'; // Default visible value should be blank
-        hide(colorOptions[0], colorOptions[1], colorOptions[2]);
-        show(colorOptions[3], colorOptions[4], colorOptions[5]);
+        hide(colorOptions[1], colorOptions[2], colorOptions[3]);
+        show(colorOptions[4], colorOptions[5], colorOptions[6]);
     } else if (design.value === 'js puns') {
-        show(colorDiv);
-        color.value = 'default'; // Default visible value should be blank
-        hide(colorOptions[3], colorOptions[4], colorOptions[5]);
-        show(colorOptions[0], colorOptions[1], colorOptions[2]);
+        hide(colorOptions[4], colorOptions[5], colorOptions[6]);
+        show(colorOptions[1], colorOptions[2], colorOptions[3]);
     } else {
-        hide(colorDiv);
-
+        hide(colors);
     }
 });
-/**********************************************************
-Register for Activities
-*********************************************************/
-let activities = fieldset[2];
-let checkboxes = activities.querySelectorAll('input');
+
+/******************************************
+REGISTER FOR ACTIVITIES
+******************************************/
+
 activities.append(div);
 activities.addEventListener('change', e => { // Listen for changes to the fieldset that includes all checkboxes
     let targetItem = e.target;
@@ -116,113 +126,123 @@ activities.addEventListener('change', e => { // Listen for changes to the fields
 
     }
 });
-/**********************************************************
-Payment Info
- *********************************************************/
-let payment = fieldset[3];
-let paymentMethod = document.getElementById('payment');
-let creditCardText = document.getElementById('credit-card'); // Select the appropriate text for each case
-let paypalText = document.getElementById('paypal');
-let bitcoinText = document.getElementById('bitcoin');
-paymentMethod[1].selected = true; // By default set non-credit card items to hidden
-hide(paypalText, bitcoinText);
 
-payment.addEventListener('change', e => { // Listen for a change in the selection input and react accordingly
-    if (paymentMethod.value === 'credit card') {
-        hide(paypalText, bitcoinText);
-        show(creditCardText);
-    } else if (paymentMethod.value === 'paypal') {
-        hide(creditCardText, bitcoinText);
-        show(paypalText);
-    } else if (paymentMethod.value = 'bitcoin') {
-        hide(creditCardText, paypalText);
-        show(bitcoinText);
+/******************************************
+PAYMENT INFO
+******************************************/
+
+payment.selectedIndex = 1; // By default set non-credit card items to hidden
+payment.options[0].disabled = true;
+hide(paypal, bitcoin);
+
+payment.addEventListener('change', e => { // Listen for a change in the selection input and react based on the selected value
+    if (payment.value === 'credit card') {
+        hide(paypal, bitcoin);
+        show(credit);
+    } else if (payment.value === 'paypal') {
+        hide(credit, bitcoin);
+        show(paypal);
+    } else if (payment.value = 'bitcoin') {
+        hide(credit, paypal);
+        show(bitcoin);
     } else {
-        paymentMethod.value = 'credit card';
-        hide(paypalText, bitcoinText);
-        show(creditCardText);
+        payment.value = 'credit card';
+        hide(paypal, bitcoin);
+        show(credit);
     }
 });
 /**********************************************************
- Validation
+VALDIATION RULES
  *********************************************************/
 
-let form = document.querySelector('form');
-form.addEventListener("submit", validate);
+let validName = () => { // Name cannot be blank
+    let value = name.value;
+    if (value == null || value == "") {
+        name.className = 'field-error';
 
-function validate() {
+        return false;
+    } else {
+        name.removeAttribute('class');
+        return true;
+    }
+};
 
-    let submittedName = document.getElementById('name').value; // user submitted text for name field
-    let userEmail = document.getElementById('mail');
-    let submittedEmail = userEmail.value; // user submitted text for email field
-    let submittedPayment = document.getElementById('payment').value; // user submitted value for payment selection field
-    let userCvv = document.getElementById('cvv');
-    let submittedCvv = userCvv.value;
-    let userZip = document.getElementById('zip');
-    let submittedZip = userZip.value;
-    let userCc = document.getElementById('cc-num');
-    let submittedCc = userCc.value;
-
+let validEmail = () => { // Email must be valid syntax (name@name.com)
+    let value = mail.value;
     let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (value == null || value == "" || !regex.test(value)) {
+        mail.className = "field-error";
+        return false;
+    } else {
+        mail.removeAttribute('class');
+        return true;
+    }
+};
+
+let validActivities = () => { // At least 1 activity must be selected
     let boxesSelected = 0;
-
-
     for (i = 0; i < checkboxes.length; i++) {
         if (checkboxes[i].checked === true) {
             boxesSelected++;
         }
     }
+    if (boxesSelected == 0) {
+        actError.className = "error-text"
+        actError.innerHTML = "Please select at least 1 activity"
+        activities.prepend(actError);
 
-    if (submittedName.length < 1) { // Check for a blank name field
-        userName.style.borderColor = 'red';
+        return false;
+    } else {
+        return true;
     }
+};
 
-    if (!regex.test(submittedEmail)) { // Check for properly formatted email address
-        userEmail.style.borderColor = 'red';
+let validNum = () => { // Credit card number must be between 13 and 16 numerical characters 
+    if (isNaN(ccNum.value) || ccNum == "" || ccNum.value.length > 16 || ccNum.value.length < 13) {
+        ccNum.className = "field-error";
+        return false;
+    } else {
+        ccNum.removeAttribute('class');
+        return true;
     }
-
-    if (boxesSelected < 1) { // Check to make sure at least one activity is selected
-        console.log('Must select at least one activity');
-    }
-
-    if (submittedPayment === 'credit card' && (!submittedCc > 11 || submittedCc < 16) && (submittedCvv.length !== 3 || submittedZip.length !== 5)) {
-        userCvv.style.borderColor = 'red';
-        userZip.style.borderColor = 'red';
-        event.preventDefault();
-
-    }
-
-
-
 }
 
+let validZip = () => { // Zip code must be exactly 5 numerical characters
+    if (isNaN(zip.value) || zip.value.length !== 5) {
+        zip.className = "field-error";
+        return false;
+    } else {
+        zip.removeAttribute('class');
+        return true;
+    }
+}
 
+let validCVV = () => { // CVV must be exactly 3 numerical characters
+    if (isNaN(cvv.value) || cvv.value.length !== 3) {
+        cvv.className = "field-error";
+        return false;
+    } else {
+        cvv.removeAttribute('class');
+        return true;
+    }
+}
 
+/******************************************
+REAL TIME
+******************************************/
+mail.addEventListener('focus', validName);
+title.addEventListener('focus', validEmail);
+zip.addEventListener('focus', validNum);
+cvv.addEventListener('focus', validZip);
+expMonth.addEventListener('focus', validCVV);
+/******************************************
 
+******************************************/
 
+form.addEventListener('submit', (e) => {
 
+    if (!validName() || !validEmail() || !validActivities() || !validNum() || !validZip() || !validCVV()) {
+        e.preventDefault();
+    }
 
-
-
-/**********************************************************
- Regression Tests for Grading
- *********************************************************/
-
-// True — Focus on the first field
-// True — "Your job role" text field appears when user selects "Other" from the Job Role menu.
-// True — Until a theme is selected from the “Design” menu, no color options appear in the “Color” drop down and the “Color” field reads “Please select a T-shirt theme”.
-// True — When a new theme is selected from the "Design" menu, the "Color" field and drop down menu is updated.
-// True — “Color” drop down menu is hidden until a T-Shirt design is selected.
-// True — User cannot select two activities that are at the same time.
-// True — Total cost of selected activities is calculated and displayed below the list of activities.
-// True — The "Credit Card" payment option is selected by default.
-// True — Payment option in the select menu matches the payment option displayed on the page.
-// True — When a user chooses a payment option, the chosen payment section is revealed and the other payment sections are hidden.
-// True — Form cannot be submitted if name is blank.
-// True — Form cannot be submitted if email field isn't formatted correctly.
-// True — Form cannot be submitted if no activities are selected.
-//      — Form cannot be submitted if credit card is selected and CC# isn't 13-16 digits.
-//      — Form cannot be submitted if credit card is selected and zipcode isn't 5 digits.
-//      — Form cannot be submitted if credit card is selected and CVV isn't 3 digits.
-//      — At least one field does real time data validation.
-//      — Without JavaScript all form fields and payment information is displayed.
+});
