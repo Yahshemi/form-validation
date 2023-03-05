2920b0c8e7655c86be4e626ba59b5264e753286a
yahshemi <yahshemi@gmail.com>
2023-03-05 12:21:39 -0800

    fix: link in index.js

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
 const server = require("./server");
 const PORT = process.env.PORT || 8080;
 
-server.listen(PORT, ()=> console.log(`server is listening on ${PORT}`));
+server.listen(PORT, ()=> console.log(`server is listening on http://localhost:${PORT}`));
